76875e4ae3c2161ff79fb910e3c75c2256c93ac7
David <felloous@gmail.com>
2023-02-19 12:56:03 -0300

    3° exercise adjustment

--- Leetcode/3_is_isomorphic.cpp
+++ Leetcode/3_is_isomorphic.cpp
@@ -7,18 +7,16 @@ public:
     bool isIsomorphic(std::string s, std::string t) {
         char map_s[256] = {0}, map_t[256] = {0};
         char s_index, t_index; 
-        if(s.size() == t.size()) {
-            for(size_t i = 0; i < s.size(); i++) {
-                if(map_s[s[i]] && map_s[s[i]] != t[i])
-                    return false; 
-                if(map_t[t[i]] && map_t[t[i]] != s[i])
-                    return false; 
-                map_s[s[i]] = t[i];
-                map_t[t[i]] = s[i]; 
-            } 
-        return true; 
-        }
-        return false; 
-    }
 
-};
+        for(size_t i = 0; i < s.size(); i++) {
+            if(map_s[s[i]] && map_s[s[i]] != t[i])
+                return false; 
+            if(map_t[t[i]] && map_t[t[i]] != s[i])
+                return false; 
+                
+            map_s[s[i]] = t[i];
+            map_t[t[i]] = s[i]; 
+        } 
+        return true; 
+    }
+};  
