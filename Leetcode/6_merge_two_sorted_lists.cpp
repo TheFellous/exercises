563e7bd6f1d36ac946144016ce7e518181f29e3c
David <felloous@gmail.com>
2023-02-24 16:42:33 -0300

    6° exercise Leetcode - merge 2 sorted lists

---
+++ Leetcode/6_merge_two_sorted_lists.cpp
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/reverse-linked-list/description/?envType=study-plan&id=level-1
+// 24/02/23
+#include <iostream>
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* aux = new ListNode();
+        ListNode* forward   = aux;
+            
+        while(list1 != nullptr && list2 != nullptr) {
+            if(list1->val < list2->val) 
+                forward->next = new ListNode(list1->val), 
+                forward = forward->next, 
+                list1 = list1->next; 
+            else 
+                forward->next = new ListNode(list2->val), 
+                forward = forward->next,
+                list2 = list2->next; 
+        }
+        if(list1 == nullptr) 
+            forward->next = list2; 
+        else
+            forward->next = list1; 
+               
+        return aux->next; 
+    }
+};
