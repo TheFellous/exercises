9e972eb6054b1ad740e7dd1a9d5e2d0c0c161b7a
David <felloous@gmail.com>
2023-02-24 09:48:47 -0300

    5° exercise LeetCode

---
+++ Leetcode/5_reverse_linked_list.cpp
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/reverse-linked-list/description/?envType=study-plan&id=level-1
+// 24/02/23
+#include <iostream>
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+    
+    ListNode *backward = nullptr; 
+    ListNode *current = head; 
+    ListNode *new_head = nullptr; 
+    while(current != nullptr) {
+
+        if(current->next == nullptr)
+            new_head = current; 
+
+        ListNode *foreward = current->next;         
+        current->next = backward;
+        backward = current; 
+        current = foreward;
+        
+    }
+    return new_head; 
+    }
+};
