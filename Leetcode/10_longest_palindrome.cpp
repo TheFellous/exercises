0b57ef37a8a39a5a03e70b71ed2a5d34ae171536
David <felloous@gmail.com>
2023-02-27 16:53:37 -0300

    10° Exercise - longest palindrome

---
+++ Leetcode/10_longest_palindrome.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+class Solution {
+public:
+    int longestPalindrome(std::string s) {   
+
+        char map[128]{};
+        int counter{0}, aux{0};  
+        for(size_t i = 0; i < s.size(); i++) {
+            if(map[s[i]] == s[i]) {
+                map[s[i]] = 0;
+                counter+= 2 ; 
+                aux--; 
+            }
+            else {
+            map[s[i]] = s[i];
+            aux++; 
+        } 
+    }
+    if (aux > 0)
+        return counter + 1; 
+    
+    return counter; 
+    }
+};
