1e896517e072818caa498f397b7394720a83a3c8
David <felloous@gmail.com>
2023-03-01 17:07:03 -0300

    15° exercise - is it  a valid BST?

---
+++ Leetcode/15_is_valid_BST.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/validate-binary-search-tree/submissions/907340359/?envType=study-plan&id=level-1
+// 01/03/23
+ #include <iostream>
+ #include <vector>
+ // Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) 
+    {
+        std::vector<int> compare; 
+        travel(root, compare); 
+        for(int i = 0; i < compare.size() - 1; i++)
+            if(compare[i+1] <= compare[i])
+                return false; 
+        return true; 
+
+    }
+private:
+    void travel(TreeNode* root, std::vector<int> &holder)
+    {
+        if(!root) return; 
+        
+        travel(root->left, holder); 
+        holder.push_back(root->val);
+        travel(root->right, holder);      
+    }
+};
